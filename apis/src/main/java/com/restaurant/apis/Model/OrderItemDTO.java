a5ec927c7cd3881771c481388b2a9b20aee0e573
HiteshAsranii <hitesh.asrani224@gmail.com>
2024-10-24 15:57:08 +0530

    folder rename

--- apis/src/main/java/com/restaurant/apis/Model/OrderItemDTO.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.apis.model;
-
-import lombok.Data;
-
-@Data
-public class OrderItemDTO {
-    private String menuItemName;
-    private int quantity;
-}
--- apis/src/main/java/com/restaurant/apis/Model/YearlySales.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.apis.model;
-
-import lombok.Data;
-
-@Data
-public class YearlySales {
-    private int year;
-    private double amount;
-}
